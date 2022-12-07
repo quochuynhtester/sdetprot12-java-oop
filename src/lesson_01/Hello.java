93bfe7fe9124b7635161703193cb848f8219c23d
quoc huynh <huynhquoc.tester@gmail.com>
2022-12-07 11:11:04 +0700

    adđ Variable lesson

--- src/lesson_01/Hello.java
+++ src/lesson_01/Hello.java
@@ -4,4 +4,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello!");
     }
+
 }
---
+++ src/lesson_01/variableLesson.java
@@ -0,0 +1,15 @@
+package lesson_01;
+
+public class variableLesson {
+    public static void main(String[] args) {
+        short myNum = 128;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+    }
+}
